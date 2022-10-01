2145979df32ed6f237eae7cac11fc2cb412e07be
Zakaria Chatouane <zack.ch.official@gmail.com>
2022-10-01 13:56:50 +0100

    add i18n

--- rachadoc/core/urls.py
+++ rachadoc/core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path("overseas/", admin.site.urls),
 ]
+
+if "rosetta" in settings.INSTALLED_APPS:
+    urlpatterns += [re_path(r"^rosetta/", include("rosetta.urls"))]
+
+# urlpatterns += i18n_patterns()
