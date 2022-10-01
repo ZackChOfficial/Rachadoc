9ff9787a013b210d3bf2ca51b178f8c3b3e31af1
Zakaria Chatouane <zack.ch.official@gmail.com>
2022-10-01 13:57:16 +0100

    format files

--- rachadoc/accounts/admin.py
+++ rachadoc/accounts/admin.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 from accounts.models import User
 
+
 class BaseUserAdmin(DjangoUserAdmin):
     """Define admin model for custom User model with no email field."""
 
@@ -23,11 +24,10 @@ class BaseUserAdmin(DjangoUserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    add_fieldsets = (
-        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
-    )
+    add_fieldsets = ((None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),)
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
+
 
 admin.site.register(User, BaseUserAdmin)
