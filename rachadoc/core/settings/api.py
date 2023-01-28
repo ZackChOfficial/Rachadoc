e1c0d5d3d5b3ecff5694e24e09e6665c8a500185
Zakaria Chatouane <zack.ch.official@gmail.com>
2023-01-28 19:33:27 +0100

    add django-filter and orjson and pagination

--- rachadoc/core/settings/api.py
+++ rachadoc/core/settings/api.py
@@ -9,13 +9,23 @@ REST_FRAMEWORK = {
         "djangorestframework_camel_case.parser.CamelCaseFormParser",
         "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
     ),
-    "DEFAULT_AUTHENTICATION_CLASSES": ("oauth2_provider.contrib.rest_framework.OAuth2Authentication",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 6,
 }
 
 OAUTH2_PROVIDER = {
+    "OAUTH2_BACKEND_CLASS": "oauth2_provider.oauth2_backends.JSONOAuthLibCore",
     # this is the list of available scopes
-    "SCOPES": {"read": "Read scope", "write": "Write scope"}
+    "SCOPES": {"read": "Read scope", "write": "Write scope"},
 }
