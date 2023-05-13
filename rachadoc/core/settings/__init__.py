f36d37f0967b71a45f4b3aad8a53be72710c4077
Zakaria Chatouane <zack.ch.official@gmail.com>
2023-05-13 16:29:46 +0100

    add vercel to cors

--- rachadoc/core/settings/__init__.py
+++ rachadoc/core/settings/__init__.py
@@ -195,7 +195,11 @@ CHANNEL_LAYERS = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ALLOWED_ORIGINS = ["https://rachadoc.com", "https://main.d1ok0fb36tx310.amplifyapp.com"]
+CORS_ALLOWED_ORIGINS = [
+    "https://rachadoc.com",
+    "https://main.d1ok0fb36tx310.amplifyapp.com",
+    "https://rachadoc.vercel.app",
+]
 
 
 EMAIL_BACKEND = "anymail.backends.sendinblue.EmailBackend"
