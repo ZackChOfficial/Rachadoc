1b52dae8cce5804d345c047493738341b8ea4abe
zakaria chatouane/s <zack.ch.official@gmail.com>
2023-05-16 19:45:54 +0000

    update CORS

--- rachadoc/core/settings/__init__.py
+++ rachadoc/core/settings/__init__.py
@@ -197,8 +197,12 @@ CHANNEL_LAYERS = {
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = [
     "https://rachadoc.com",
-    "https://www.rachadoc.com" "https://main.d1ok0fb36tx310.amplifyapp.com",
+    "https://www.rachadoc.com",
+    "https://main.d1ok0fb36tx310.amplifyapp.com",
     "https://rachadoc.vercel.app",
+    "http://localhost:5173",
+    "api.rachadoc.com",
+    "https://api.rachadoc.com",
 ]
 
 
